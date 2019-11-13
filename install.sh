shvchk/poly-light

commit 6709076139531612ffa26fb0bfcb92eff78e28b6

Suitable grub update method
    ** **
    Use UEFI to start, the default grub.cfg is in '/boot/efi/efi/$ID/',
    add a judgment to select the appropriate update method.
    ** **

    ** **
    This modification applies to redhat and it's modified version.
    ** **

Signed-off-by: 弱弱的胖橘猫丷 <gesangtome@foxmail.com>

install.sh
@@ -41,6 +41,12 @@ done < /dev/tty
 GRUB_DIR='grub'
 UPDATE_GRUB=''
 
+# Detect bootloader
+if [ -d "/boot/efi" ]; then
+    echo "The current bootloader is UEFI"
+    UEFI_MODE=YES
+fi
+
 if [ -e /etc/os-release ]; then
 
     source /etc/os-release
@@ -59,7 +65,11 @@ if [ -e /etc/os-release ]; then
             "$ID_LIKE" =~ (fedora|rhel|suse) ]]; then
 
         GRUB_DIR='grub2'
-        UPDATE_GRUB='grub2-mkconfig -o /boot/grub2/grub.cfg'
+        if [ "$UEFI_MODE" == "YES" ]; then
+             UPDATE_GRUB='grub2-mkconfig -o /boot/efi/EFI/$ID/grub.cfg'
+        else
+             UPDATE_GRUB='grub2-mkconfig -o /boot/grub2/grub.cfg'
+        fi
     fi
 fi
 

commit 59c26617f001ef5e5597a0bfe0fc5c3c06518df9

Fix linux shell interpretation exception
  ** **
  The modification in commit 6709076139531612ffa26fb0bfcb92eff78e28b6
  caused the linux shell error to explain the variable "$ID",
  Replace ' with " to fix the error.
  ** ** 

Signed-off-by: 弱弱的胖橘猫丷 <gesangtome@foxmail.com>

install.sh
@@ -66,7 +66,7 @@ if [ -e /etc/os-release ]; then
 
         GRUB_DIR='grub2'
         if [ "$UEFI_MODE" == "YES" ]; then
-             UPDATE_GRUB='grub2-mkconfig -o /boot/efi/EFI/$ID/grub.cfg'
+             UPDATE_GRUB="grub2-mkconfig -o /boot/efi/EFI/$ID/grub.cfg"
         else
              UPDATE_GRUB='grub2-mkconfig -o /boot/grub2/grub.cfg'
         fi